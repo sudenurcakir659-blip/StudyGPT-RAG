7b61629d76f6e5b1335e99b948a035bfcc590710
sudenurcakir659-blip <sudenurcakir659@gmail.com>
2026-07-15 21:25:23 +0300

    Update rag.py

--- rag.py
+++ rag.py
@@ -30,8 +30,7 @@ client = genai.Client(api_key=api_key)
 # =====================================================
 
 MODELS = [
-    "gemini-flash-latest",
-    "gemini-flash-lite-latest",
+    
     "gemini-2.0-flash",
     "gemini-2.0-flash-lite",
 ]
